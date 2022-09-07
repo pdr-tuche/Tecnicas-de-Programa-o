d5c3182256439af195cacd49b11903ac3bb6674a
pdr-tuche <contato.nevespedro@gmail.com>
2022-09-06 22:02:54 -0300

    exercicios da aula 06 09

---
+++ ExercíciosSala/Aula0609/Laços/questao12/whileSalarios.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+//quantos usuarios tem na empresa e receba o salario de cada um dos funcionarios ]ç calc a afolha de pagamento mensal da empresa
+int main(){
+    int quantidade;
+    float acumulador=0;
+    float salario;
+    int contador4k=0;
+    printf("digite quantos funcionarios\n");
+    scanf("%d", &quantidade);
+
+    int i =0 ;
+    while ( i<quantidade){
+        printf("digite o salario\n");
+        scanf("%f", &salario);
+        acumulador+=salario;
+        if(salario >= 4000){
+            contador4k++;
+        }
+         i++;
+    }
+    printf ("folha de pagamento: %f \n", acumulador);
+    printf("%d salarios acima de 4k \n", contador4k);
+    return 0;
+}
